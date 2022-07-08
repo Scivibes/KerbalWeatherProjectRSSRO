f9894de9012c0a826e87d88918a773e7096e7b4a
Scivibes <49595585+Scivibes@users.noreply.github.com>
2022-07-07 22:17:12 -0500

    Update atmosphere hight
    Changed from 70km to 140km for RSS/RO

--- KerbalWeatherProject/climate_data.cs
+++ KerbalWeatherProject/climate_data.cs
@@ -119,7 +119,7 @@ namespace KerbalWeatherProject
             {
                 heights[j] = heights_3d[lat_idx, lng_idx, j];
             }
-            heights[NZ-1] = 70000;
+            heights[NZ-1] = 140000;
             int hidx = Util.find_min(heights, vheight); //index of nearest height
                     
             if (hidx >= heights.Length)
